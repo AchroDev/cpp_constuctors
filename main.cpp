4702772fae1e4ba575302045395234b087580a09
AchroDev <achrodev@proton.me>
2024-09-22 17:21:44 -0500

    Added default constructor comment

--- main.cpp
+++ main.cpp
@@ -14,11 +14,9 @@ public:
         Y = 0.0f;
     }
 
-    // // example initializer method
-    // void Init()
+    // without defining a constructor you still have a default constructor that does nothing, ex:
+    // Entity()
     // {
-    //     X = 0.0f;
-    //     Y = 0.0f;
     // }
 
     // example print method
@@ -32,15 +30,8 @@ int main()
 {
     // instantiating the Entity class
     Entity e;
-    // e.Init(); // no longer needed with the constructor
-    // Trying to print the X variable, now prints 0
     std::cout << e.X << std::endl;
     // calling the print method
-    e.Print(); // now with the init, we see the values being printed correctly
-
-    // /* The problem is that now with each new entity you would have to initialize it every time, like so */
-    // Entity e1;
-    // e1.Init(); no longer needed with the constructor
-
+    e.Print();
     std::cin.get();
 }
