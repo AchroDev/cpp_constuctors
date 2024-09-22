2406055d4115d34a263ed26d023668e04000d1a9
AchroDev <achrodev@proton.me>
2024-09-22 17:15:15 -0500

    Added Entity e1

--- main.cpp
+++ main.cpp
@@ -30,5 +30,10 @@ int main()
     std::cout << e.X << std::endl;
     // calling the print method
     e.Print(); // now with the init, we see the values being printed correctly
+
+    /* The problem is that now with each new entity you would have to initialize it every time, like so */
+    Entity e1;
+    e1.Init();
+
     std::cin.get();
 }
