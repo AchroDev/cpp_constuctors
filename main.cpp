a89adac23f0da95076498ff8a37b6436744206ec
AchroDev <achrodev@proton.me>
2024-09-22 17:19:57 -0500

    Now using the constructor

--- main.cpp
+++ main.cpp
@@ -7,12 +7,19 @@ class Entity
 public:
     float X, Y;
 
-    // example initializer method
-    void Init()
+    // Constructor, using the class name - no parameters in this example
+    Entity()
     {
         X = 0.0f;
         Y = 0.0f;
     }
+
+    // // example initializer method
+    // void Init()
+    // {
+    //     X = 0.0f;
+    //     Y = 0.0f;
+    // }
 
     // example print method
     void Print()
@@ -25,15 +32,15 @@ int main()
 {
     // instantiating the Entity class
     Entity e;
-    e.Init(); // now initializing the entity
+    // e.Init(); // no longer needed with the constructor
     // Trying to print the X variable, now prints 0
     std::cout << e.X << std::endl;
     // calling the print method
     e.Print(); // now with the init, we see the values being printed correctly
 
-    /* The problem is that now with each new entity you would have to initialize it every time, like so */
-    Entity e1;
-    e1.Init();
+    // /* The problem is that now with each new entity you would have to initialize it every time, like so */
+    // Entity e1;
+    // e1.Init(); no longer needed with the constructor
 
     std::cin.get();
 }
