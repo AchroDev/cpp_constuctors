59a18a83e5afa4ce31c872164d2066de161f7c5e
AchroDev <achrodev@proton.me>
2024-09-22 17:08:24 -0500

    Trying to print X without init

--- main.cpp
+++ main.cpp
@@ -18,7 +18,9 @@ int main()
 {
     // instantiating the Entity class
     Entity e;
+    // Trying to print the X variable, will not compile as it hasn't been initialized yet
+    std::cout << e.X << std::endl;
     // calling the print method
-    e.Print(); // currently prints seemingly random values, with no definition they carry the memory value
+    e.Print(); // currently prints seemingly random values, with no definition/initialization they carry the memory value
     std::cin.get();
 }
